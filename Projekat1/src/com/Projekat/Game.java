1d9b8330fcbe45895ac60d9824d47f1da517fe52
Marko Tomić <119930643+markotomic13@users.noreply.github.com>
2025-10-02 11:10:22 +0200

    Add files via upload

---
+++ Projekat1/src/com/Projekat/Game.java
@@ -0,0 +1,16 @@
+package com.Projekat;
+
+//Jovan Perosevic 24/047; Marko Tomic 19/011; Erdan Franca 22/005;
+
+public class Game {
+
+	public static void main(String[] args) {
+		
+		Player igrac = new Player();
+		Enemy neprijatelj = new Enemy();
+		//Ovdje smo kreirali objekte
+		
+
+	}
+
+}
---
+++ Projekat1/src/com/Projekat/Player.java
@@ -0,0 +1,57 @@
+package com.Projekat;
+
+//Jovan Perosevic 24/047; Marko Tomic 19/011; Erdan Franca 22/005;
+
+public class Player {
+
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int health;
+	private int damage;
+	
+	//Ovdje smo ubacili atribute klase player
+	
+	public Player (int x, int y, int width, int height, int health, int damage) {
+		int koordinataX = x;
+		int koordinataY = y;
+		int sirina = width;
+		int visina = height;
+		int zdravlje = health;
+		int napad = damage;
+		}
+	//Metoda klase
+	
+	public int getKoordinataX() {return x;}
+	public int getKoordinataY() {return y;}
+	public int getSirina() {return width;}
+	public int getVisina() {return height;}
+	public int getZdravle() {return health;}
+	public int Napad() {return damage;}
+	//Geteri
+	
+	public void setKoordinataX(int x) {
+		x = x;
+	}
+	public void setKoordinataY(int y) {
+		y = y;
+	}
+	public void setSirina(int width) {
+		width = width;
+	}
+	public void setVisina(int height) {
+		height = height;
+	}
+	public void setZdravlje(int health) {
+		health = health;
+	}
+	public void setNapad(int damage) {
+		damage = damage;
+	}
+	//Setteri
+	
+	
+	}
+
+
